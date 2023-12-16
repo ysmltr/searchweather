7accf400b66f1f51b1be2a0930a130748bebfbff
ysmltr <yesimltr1@gmail.com>
2023-12-16 11:02:27 +0300

    Refactor weather API configuration

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,38 @@
-const url='https://api.openweathermap.org/data/2.5'
-
-const key='e58822d79181a82d00d774a4b516da14'
+require('dotenv').config();
 
 
+const apiKey = process.env.API_KEY;
+const apiUrl = process.env.API_URL; // .env dosyanızda API_URL varsa
+const isDebugMode = process.env.DEBUG === 'true';
 
-const setQuery=(e)=>{
-    if (e.keyCode=='13') {
+const setQuery = (e) => {
+    if (e.keyCode === '13') {
         getResult(searchBar.value);
-        
     }
 }
 
-
-const getResult=(cityName)=>{
-    let query = `${url}/weather?q=${cityName}&appid=${key}&units=metric&lang=en`;
+const getResult = (cityName) => {
+    let query = `${apiUrl}/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=en`;
     fetch(query)
-    .then(weather=>{
-        return weather.json()
-    })
-    .then(displayResult)
+        .then(weather => {
+            return weather.json();
+        })
+        .then(displayResult);
 }
 
-const displayResult = (result)=>{
-    let city=document.querySelector('.city');
-    city.innerText=`${result.name},${result.sys.country}`;
+const displayResult = (result) => {
+    let city = document.querySelector('.city');
+    city.innerText = `${result.name},${result.sys.country}`;
 
-    let temp=document.querySelector('.temp');
-    temp.innerText=`${Math.round(result.main.temp)}°C`;
+    let temp = document.querySelector('.temp');
+    temp.innerText = `${Math.round(result.main.temp)}°C`;
 
-    let desc=document.querySelector('.desc');
-    desc.innerText= result.weather[0].description
+    let desc = document.querySelector('.desc');
+    desc.innerText = result.weather[0].description;
 
-    let minMax=document.querySelector('.minmax');
-    minMax.innerText=`${Math.round(result.main.temp_min)}°C /  ${Math.round(result.main.temp_max)}°C `
-
+    let minMax = document.querySelector('.minmax');
+    minMax.innerText = `${Math.round(result.main.temp_min)}°C /  ${Math.round(result.main.temp_max)}°C `;
 }
 
-
-
-
-const searchBar=document.getElementById('searchBar')
-searchBar.addEventListener('keypress',setQuery);
+const searchBar = document.getElementById('searchBar');
+searchBar.addEventListener('keypress', setQuery);
